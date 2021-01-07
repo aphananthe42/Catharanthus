c93155ba246ec4ccb02dfab69c455dd7581708a1
Ryota Miyazaki <>
2021-01-07 22:48:08 +0900

    RxSwiftを使った簡単なインクリメントサンプルを実装

---
+++ DailyCoding/RxSwift-Practice/RxSwift_PracticeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RxSwift-PracticeViewController.swift
+//  DailyCoding
+//
+//  Created by Aphananthe on 2021/01/07.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class RxSwift_PracticeViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    private let count: BehaviorRelay<Int> = BehaviorRelay(value: 0)
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindButtonToValue()
+        bindCountToText()
+    }
+    
+    private func bindButtonToValue() {
+        button.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.increment()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func increment() {
+        count.accept(count.value + 1)
+    }
+    
+    private func bindCountToText() {
+        count.asObservable()
+            .map { String($0) }
+            .bind(to: label.rx.text)
+            .disposed(by: disposeBag)
+    }
+
+}
--- DailyCoding/ViewController.swift
+++ DailyCoding/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let storyboardIdentifier = "RxSwift_Practice"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func pushToEachViewController(_ sender: Any) {
+        let storyboard = UIStoryboard(name: storyboardIdentifier, bundle: nil)
+        guard let vc = storyboard.instantiateInitialViewController() as? RxSwift_PracticeViewController else { return }
+        present(vc, animated: true, completion: nil)
+    }
+    
 }
+
 
